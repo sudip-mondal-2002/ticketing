b2284eff0e0f71242c1729ae325d39d6acad6e6d
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-06-28 15:20:39 +0530

    update cookie securiy policy

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -15,7 +15,7 @@ app.set('trust proxy', true);
 app.use(express.json())
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false
 }))
 
 app.use(currentuserRoute)
